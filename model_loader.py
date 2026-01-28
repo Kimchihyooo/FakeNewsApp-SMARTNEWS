98d4ad0f568cae40c42fcef17f87f3062219d974
Josh Melvin Lazaro <joshlazaro1112@gmail.com>
2026-01-28 20:40:14 +0800

    for deployment

--- model_loader.py
+++ model_loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1463f9b29178f0a5215e7c7b29188cb87132384128d81e8d8254a7d75cc0926b
-size 7841
+oid sha256:4f0977f7131df123b80cf57781468fbade49a64d9251b3fcfcb1cc66ff41e2fc
+size 8371
