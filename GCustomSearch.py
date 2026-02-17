4b8927cabac406c52c6ce7966f350e7f9d2e9326
Josh Melvin Lazaro <joshlazaro1112@gmail.com>
2026-02-17 14:09:27 +0800

    backend gcustom search

--- GCustomSearch.py
+++ GCustomSearch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:427df6feebb4458a4d254cc9960f3cdaed5923599081f08072fa4bc942089f83
-size 9343
+oid sha256:e29df6ac2b94d1ef65bb556abf0e86251284cfe512b099b5e8b2e2bf8e016fef
+size 7395
